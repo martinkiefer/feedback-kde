702e23a4e75113c8f91dd453b584fbca12d0bd2d
Max Heimel <mheimel@googlemail.com>
2014-02-14 20:06:15 +0100

    Added options to the workload generation script.

--- analysis/genhist/generateWorkload1.py
+++ analysis/genhist/generateWorkload1.py
@@ -1,12 +1,39 @@
+import argparse
 import psycopg2
 import random
+import sys
 import time
 
-# Open a connection to postgres.
-conn = psycopg2.connect("dbname=mheimel user=mheimel host=localhost")
-cur = conn.cursor()
+def usage():
+    print "Usage flags: "
+    print "\t--output=<filename> / -o=<filename>"
+    print "\t\tName of the output sql file."
+    print "\t--algorithm=<random/binary/interpolation> / -a=<random/binary/interpolation>"
+    print "\t\tWhich algorithm should be used? (Default: random)"
 
-table="tpch_data"
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--dbname", action="store", required=True, help="Database to which the script will connect.")
+parser.add_argument("--table", action="store", required=True, help="Table for which the query workload will be generated.")
+parser.add_argument("--selectivity", action="store", required=True, type=float, help="Target selectivity for the created query workload.")
+parser.add_argument("--tolerance", action="store", required=True, type=float, help="Tolerance around the target selectivity.")
+parser.add_argument("--queries", action="store", required=True, type=int, help="Number of queries in the target workload.")
+parser.add_argument("--output", action="store", required=True, help="Name of the output sql file.")
+parser.add_argument("--method", action="store", choices=["random","binary","interpolation"], default="random", help="Which method should be used to construct the queries?")
+
+args = parser.parse_args()
+
+database = args.dbname
+table = args.table
+target_selectivity = args.selectivity
+target_tolerance = args.tolerance
+queries = args.queries
+output_file = args.output
+method = args.method
+
+# Open a connection to postgres.
+conn = psycopg2.connect("dbname=%s host=localhost" % database)
+cur = conn.cursor()
 
 # First, we need to figure out the dimensionality of this table.
 cur.execute("SELECT COUNT(*) FROM information_schema.columns WHERE table_name='%s'" % table)
@@ -45,44 +72,48 @@ for i in range(0, columns):
     template += "c%d>%%s AND c%d<%%s " % (i+1, i+1)
     
 
-# Prepare container to store the queries for the different workload types.
-workload_1 = [] # Queries with selectivity ~ 1%
+workload = []
 
 last_len = 0
 last_print_time = time.time()
-
-target = 0.01
-tolerance = 0.01
 
 query = [0] * (columns * 2)
 while True:
     # Pick a starting point. 
     for i in range(0, columns):
-        query[2*i] = ranges[i][0] + random.random()*(ranges[i][1] - ranges[i][0])
-        #b = a + 0.2*random.random()*(ranges[i][1] - a)
-        query[2*i+1] = ranges[i][1]
+        if method == "random":
+            a = ranges[i][0] + random.random()*(ranges[i][1] - ranges[i][0])
+            b = ranges[i][0] + random.random()*(ranges[i][1] - ranges[i][0])
+            query[2*i] = min(a, b)
+            query[2*i+1] = max(a, b)
+        else:
+            query[2*i] = ranges[i][0] + random.random()*(ranges[i][1] - ranges[i][0])
+            query[2*i+1] = ranges[i][1]
     cur.execute(template, query)
     selectivity = cur.fetchone()[0] / float(rows)
-    if (selectivity < (target-0.5*tolerance)):
+    if (selectivity < (target_selectivity - 0.5*target_tolerance)):
       continue  # Move to the next query.
-    if (selectivity > (target+0.5*tolerance)):
-      # Now do a extrapolation search to pick a viable query range.
+    if (selectivity > (target_selectivity + 0.5*target_tolerance)):
+      if method == "random":
+          continue
+      # Adjust the query to match 
       lower_bound = 0 
       lower_bound_factor = 0 
       upper_bound = selectivity
       upper_bound_factor = 1 
       test_query = list(query)
-      while (upper_bound - lower_bound > tolerance and (upper_bound_factor - lower_bound_factor) > 0.001 ):
+      while (upper_bound - lower_bound > target_tolerance and (upper_bound_factor - lower_bound_factor) > 0.001 ):
         # Compute the projected factor.
         test_factor = lower_bound_factor + (target - lower_bound) * float(upper_bound_factor - lower_bound_factor)/float(upper_bound - lower_bound)
+        #test_factor = 0.5 * (lower_bound_factor + upper_bound_factor)
         # Evaluate the selectivity
         for i in range(0, columns):
           test_query[2*i + 1] = query[2*i] + test_factor * (query[2*i + 1] - query[2*i])
         cur.execute(template, test_query)
         test = cur.fetchone()[0] / float(rows)
-        if (test < (target+0.5*tolerance) and test > (target-0.5*tolerance)):
+        if (test < (target_selectivity + 0.5*target_tolerance) and test > (target_selectivity - 0.5*target_tolerance)):
           break;
-        elif (test > target):
+        elif (test > target_selectivity):
           upper_bound = test 
           upper_bound_factor = test_factor
         else:
@@ -90,17 +121,21 @@ while True:
           lower_bound_factor = test_factor 
       # This is a workload 1 query.
       query = list(test_query)
-    workload_1.append(list(query))
-    if (len(workload_1) == 10000): 
+    workload.append(list(query))
+    if (len(workload) == queries): 
       break
-    if (time.time() - last_print_time >= 5):
-        print "%f queries / second" % ((len(workload_1) - last_len) / float(5))
+    if (time.time() - last_print_time >= 10):
+        print "%f queries / second" % ((len(workload_1) - last_len) / float(10))
         last_print_time = time.time()
         last_len = len(workload_1)
 conn.close()
 
+# Prepare writing out the result to disk.
 template = template.replace("%s", "%f")
+template += ";\n"
 
+f = open(output_file, "w")
 # Write out the resulting workload.
-for query in workload_1:
-    print template % tuple(query)
+for query in workload:
+    f.write(template % tuple(query))
+f.close()
