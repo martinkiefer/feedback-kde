e9bb2fa0f827d95c6f8d7711cd853c0786a996bf
Martin Kiefer <kiefer@campus.tu-berlin.de>
2014-04-01 19:20:39 +0200

    Restored to version before accidential commit.

--- analysis/conf.sh
+++ analysis/conf.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 
 # Postgres specific variables:
-PSQL="/usr/local/pgsql/bin/psql"
-PGDATABASE="xy"
+PSQL="psql"
+PGDATABASE="mheimel"
 
 # MonetDB specific variables:
 MONETDATABASE=""
@@ -11,4 +11,4 @@ MONETDATABASE=""
 COVTYPE_FILE=""	# Can be left empty (Script will download the data file instead.)
 
 # Variables for the Genhist TPCH dataset:
-DBGEN_FOLDER="/home/martin/Dokumente/HiWi/Data_Generation/dbgen" # Required.
+DBGEN_FOLDER="/home/mheimel/tpch/dbgen" # Required.
