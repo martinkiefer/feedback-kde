73bc2ae7e2cef883b2b5401736a7d52dc74c9327
Max Heimel <mheimel@googlemail.com>
2015-05-14 03:08:36 +0200

    Cleaned up experimental scripts and merged runExperiment & replayExperiment scripts.

--- analysis/static/run.sh
+++ analysis/static/run.sh
@@ -6,11 +6,9 @@ source $DIR/../conf.sh
 # Some general parameters.
 REPETITIONS=10
 TRAINQUERIES=100
-QUERIES=300
-STHOLES_MODELSIZE=512
-KDE_MODELSIZE=1024
-ERROR=absolute
-LOGFILE=$DIR/result_new.csv
+TESTQUERIES=300
+MODELSIZE=1024
+LOGFILE=$DIR/result.csv
 
 # Prepare a new result file.
 echo > $LOGFILE
@@ -22,58 +20,80 @@ for dataset in $DIR/datasets/*; do
     for query in $dataset/queries/*; do
         [ -f "${query}" ] || continue
         query_file=`basename $query`
-        echo "  Running query $query_file:"
+        echo "  Running for query file $query_file:"
         # Reinitialize postgres (just to be safe)
         postgres -D $PGDATAFOLDER -p $PGPORT > postgres.log 2>&1 &
         PGPID=$!
         sleep 2
         for i in $(seq 1 $REPETITIONS); do
            echo "    Repetition $i:"
+           TS=$SECONDS
 
-           # Pick a new experiment (and run batch).
-           echo "      KDE batch:"
-           python $DIR/runExperiment.py                        \
-              --dbname=$PGDATABASE --port=$PGPORT              \
-              --queryfile=$query --log=$LOGFILE                \
-              --model=kde_batch --modelsize=$KDE_MODELSIZE     \
-              --trainqueries=$TRAINQUERIES --queries=$QUERIES  \
-              --error=$ERROR --record
-
-           # Run stholes:  
-           echo "      STHoles:"
-           python $DIR/replayExperiment.py                     \
-              --dbname=$PGDATABASE --port=$PGPORT              \
-              --model=stholes --modelsize=$STHOLES_MODELSIZE   \
-              --error=$ERROR --log=$LOGFILE
+           # Pick a new experiment and run batch:
+           echo "      KDE (batch):"
+           python $DIR/runExperiment.py                                 \
+              --dbname=$PGDATABASE --port=$PGPORT                       \
+              --queryfile=$query --log=$LOGFILE                         \
+              --model=kde_batch --modelsize=$MODELSIZE                  \
+              --trainqueries=$TRAINQUERIES --testqueries=$TESTQUERIES
             
            # Run KDE heuristic: 
-           echo "      KDE heuristic:"
-           python $DIR/replayExperiment.py                     \
-              --dbname=$PGDATABASE --port=$PGPORT              \
-              --model=kde_heuristic                            \
-              --error=$ERROR --log=$LOGFILE
+           echo "      KDE (heuristic):"
+           python $DIR/runExperiment.py                                 \
+              --dbname=$PGDATABASE --port=$PGPORT                       \
+              --queryfile=$query --log=$LOGFILE                         \
+              --model=kde_heuristic --modelsize=$MODELSIZE              \
+              --trainqueries=$TRAINQUERIES --testqueries=$TESTQUERIES   \
+              --replay_experiment
             
            # Run KDE optimal: 
-           echo "      KDE optimal:"
-           python $DIR/replayExperiment.py                     \
-              --dbname=$PGDATABASE --port=$PGPORT              \
-              --model=kde_optimal                              \
-              --error=$ERROR --log=$LOGFILE
+           echo "      KDE (SCV):"
+           python $DIR/runExperiment.py                                 \
+              --dbname=$PGDATABASE --port=$PGPORT                       \
+              --queryfile=$query --log=$LOGFILE                         \
+              --model=kde_scv --modelsize=$MODELSIZE                    \
+              --trainqueries=$TRAINQUERIES --testqueries=$TESTQUERIES   \
+              --replay_experiment
             
            # Run KDE adpative: 
-           echo "      KDE adaptive:"
-           python $DIR/replayExperiment.py                     \
-              --dbname=$PGDATABASE --port=$PGPORT              \
-              --model=kde_adaptive_rmsprop                     \
-              --error=$ERROR --log=$LOGFILE
+           echo "      KDE (adaptive):"
+           python $DIR/runExperiment.py                                 \
+              --dbname=$PGDATABASE --port=$PGPORT                       \
+              --queryfile=$query --log=$LOGFILE                         \
+              --model=kde_adaptive --modelsize=$MODELSIZE --logbw       \
+              --trainqueries=$TRAINQUERIES --testqueries=$TESTQUERIES   \
+              --replay_experiment
            
-           # Run KDE adpative (log-scaled): 
-           echo "      KDE adaptive (log-scaled):"
-           python $DIR/replayExperiment.py                     \
-              --dbname=$PGDATABASE --port=$PGPORT              \
-              --model=kde_adaptive_rmsprop --logbw             \
-              --error=$ERROR --log=$LOGFILE
+           # Run stholes:  
+           echo "      STHoles:"
+           python $DIR/runExperiment.py                                 \
+              --dbname=$PGDATABASE --port=$PGPORT                       \
+              --queryfile=$query --log=$LOGFILE                         \
+              --model=stholes --modelsize=$MODELSIZE                    \
+              --trainqueries=$TRAINQUERIES --testqueries=$TESTQUERIES   \
+              --replay_experiment
+
+           # Run with Postgres Histograms:
+           echo "      Postgres histograms:"
+           python $DIR/runExperiment.py                                 \
+              --dbname=$PGDATABASE --port=$PGPORT                       \
+              --queryfile=$query --log=$LOGFILE                         \
+              --model=postgres --modelsize=$MODELSIZE                   \
+              --trainqueries=$TRAINQUERIES --testqueries=$TESTQUERIES   \
+              --replay_experiment
            
+           # Run without statistics:
+           echo "      No statistics:"
+           python $DIR/runExperiment.py                                 \
+              --dbname=$PGDATABASE --port=$PGPORT                       \
+              --queryfile=$query --log=$LOGFILE                         \
+              --model=none --modelsize=$MODELSIZE                       \
+              --trainqueries=$TRAINQUERIES --testqueries=$TESTQUERIES   \
+              --replay_experiment
+           
+           ELAPSED=$(($SECONDS - $TS))
+           echo "    Repetition finished (took $ELAPSED seconds)!"
+
         done
         kill -9 $PGPID
         sleep 2
--- analysis/static/runExperiment.py
+++ analysis/static/runExperiment.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 import argparse
+from cPickle import load
 import csv
 import inspect
 import ntpath
@@ -14,15 +15,72 @@ import time
 from rpy2.robjects.packages import importr
 from rpy2 import robjects
 
-def createModel(table, dimensions, reuse):
+# Helper function to load the query workloads.
+def prepareWorkload(args):
+    # Filenames used to load / record the generated workloads.
+    trainworkload_filename = "/tmp/train_queries_%s.log" % args.dbname
+    testworkload_filename = "/tmp/test_queries_%s.log" % args.dbname
+    # Lists for the workloads.
+    testworkload = []
+    trainworkload = []
+    if args.replay_experiment: # Load the workload from the specified files:
+        f = open(trainworkload_filename)
+        for query in f:
+            trainworkload.append(query)
+        f.close()
+        f = open(testworkload_filename)
+        for query in f:
+            testworkload.append(query)
+        f.close()
+    else: # Select new testing and training workloads.
+        sys.stdout.write("\tPreparing new workload ... ")
+        sys.stdout.flush()
+        # First, count how many queries are in the queryfile.
+        f = open(args.queryfile)
+        for queries_in_file, _ in enumerate(f):
+            pass
+        queries_in_file += 1
+        f.close()
+        # Do a little sanity check:
+        if (queries_in_file < args.trainqueries):
+            print "Requested more training queries than available."
+            sys.exit(-1)
+        if (queries_in_file < args.testqueries):
+            print "Requested more testing queries than available."
+            sys.exit(-1)
+        # Select a few random queries for the training workload.
+        selected_training_queries = range(1, queries_in_file)
+        random.shuffle(selected_training_queries)
+        selected_training_queries = set(selected_training_queries[0:args.trainqueries])
+        # Select a few random queries for the test workload.
+        selected_test_queries = range(1, queries_in_file)
+        random.shuffle(selected_test_queries)
+        selected_test_queries = set(selected_test_queries[0:args.testqueries])
+        # Now walk through the query file and move the selected queries to the corresponding lists.
+        f_train = open(trainworkload_filename, "w")
+        f_test = open(testworkload_filename, "w")
+        f = open(args.queryfile)
+        for linenr, query in enumerate(f):
+            if linenr in selected_training_queries:
+                trainworkload.append(query)
+                f_train.write(query)
+            if linenr in selected_test_queries:
+                testworkload.append(query)
+                f_test.write(query)
+        f.close()
+        f_train.close()
+        f_test.close()
+        print "done!"
+    return (trainworkload, testworkload)
+
+# Helper function to build the estimator model on the given table.
+def analyzeTable(args, table, dimensions):
   query = ""
-  if reuse:
-    sys.stdout.write("\tRetraining existing estimator ... ")
-    sys.stdout.flush()
-    query += "SELECT kde_reset_bandwidth('%s');" % table
-  else:
+  if args.model != "none":
+    # Trigger reanalyzing the table.
     sys.stdout.write("\tBuilding estimator ... ")
     sys.stdout.flush()
+    ts = time.time()
     query += "ANALYZE %s(" % table
     for i in range(1, dimensions + 1):
       if (i>1):
@@ -30,198 +88,147 @@ def createModel(table, dimensions, reuse):
       else:
         query += "c%i" %i
     query += ");"
-  cur.execute(query)
-  print "done!"
+    cur.execute(query)
+    # For KDE, we want to restore the sample to avoid
+    if "kde" in args.model:
+        sample_file = "/tmp/sample_%s.csv" % args.dbname
+        if args.replay_experiment:
+            # Import the sample and make sure the bandwidth is re-optimized.
+            cur.execute("SELECT kde_import_sample('%s', '%s');" % (table, sample_file))
+            cur.execute("SELECT kde_reset_bandwidth('%s');" % table)
+        else:
+            # Export the sample.
+            cur.execute("SELECT kde_dump_sample('%s', '%s');" % (table, sample_file))
+    print "done (took %.2f ms)!" % (1000*(time.time() - ts))
 
-# Define and parse the command line arguments
+# Define and parse the command line arguments:
 parser = argparse.ArgumentParser()
+    # Database connectivity arguments.
 parser.add_argument("--dbname", action="store", required=True, help="Database to which the script will connect.")
 parser.add_argument("--port", action="store", type=int, default=5432, help="Port of the postmaster.")
-parser.add_argument("--queryfile", action="store", required=True, help="File with benchmark queries.")
-parser.add_argument("--queries", action="store", required=True, type=int, help="How many queries from the workload should be used?")
+    # Arguments to specify the experiment.
+parser.add_argument("--replay_experiment", action="store_true", help="Should the script [replay] the last recorded experiment?")
+parser.add_argument("--queryfile", action="store", help="From which file should we take the test & training queries?.")
+parser.add_argument("--testqueries", action="store", type=int, help="How many queries should be used to evalaute the model error?")
 parser.add_argument("--trainqueries", action="store", default=100, type=int, help="How many queries should be used to train the model?")
-parser.add_argument("--model", action="store", choices=["stholes", "kde_heuristic", "kde_adaptive_rmsprop","kde_adaptive_vsgd","kde_batch", "kde_optimal"], default="none", help="Which model should be used?")
-parser.add_argument("--modelsize", action="store", required=True, type=int, help="How many rows should the generated model sample?")
-parser.add_argument("--error", action="store", choices=["absolute", "relative"], default="absolute", help="Which error metric should be optimized / reported?")
+    # Arguments to specify the model.
+parser.add_argument("--model", action="store", choices=["none", "postgres", "stholes", "kde_heuristic", "kde_scv", "kde_adaptive", "kde_batch"], default="none", help="Which model should be tested?")
+parser.add_argument("--modelsize", action="store", required=True, type=int, help="For KDE: How many rows are used in the underlying sample. For STHoles: How many points are used to build the model?")
+parser.add_argument("--logbw", action="store_true", help="Use a logarithmic bandwidth representation.")
+    # General arguments
 parser.add_argument("--log", action="store", required=True, help="Where to append the experimental results?")
-parser.add_argument("--reuse", action="store_true", help="Don't rebuild the model.")
-parser.add_argument("--record", action="store_true", help="If set, the script will store the training & testing workload, actual selectivities, as well as the data sample to files in /tmp.")
-parser.add_argument("--gpu", action="store_true", help="Use the graphics card for the experiment.")
-parser.add_argument("--logbw", action="store_true", help="Use logarithmic bandwidth representation.")
 
 args = parser.parse_args()
 
-# Fetch the arguments.
-queryfile = args.queryfile
-queries = args.queries
-trainqueries = args.trainqueries
-model = args.model
-modelsize = args.modelsize
-errortype = args.error
-log = args.log
-uselogbw = args.logbw
-usegpu = args.gpu
-
-# Error log file that we will use.
-error_log = "/tmp/error_%s.log" % args.dbname
-
-# Open a connection to postgres.
+# First, open the connection to Postgres.
 conn = psycopg2.connect("dbname=%s host=localhost port=%i" % (args.dbname, args.port))
 conn.set_session('read uncommitted', autocommit=True)
 cur = conn.cursor()
 
+# Make sure that debug mode is deactivated and that all model traces are removed (unless we want to reuse the model):
 cur.execute("SET kde_debug TO false;")
+cur.execute("SET ocl_use_gpu TO false;")
+cur.execute("SET kde_error_metric TO Quadratic;")
+cur.execute("DELETE FROM pg_kdefeedback;")
 
-# Extract table name, workload, selectivity and dimensionality from the query file name.
-queryfilename = ntpath.basename(queryfile)
-m = re.match("(.+)_([a-z]+)_(.+).sql", queryfilename)
+# Now prepare the workloads:
+(training_workload, testing_workload) = prepareWorkload(args)
+
+# Extract the name of the table, its dimensionality and the workload type
+m = re.search("FROM (.+) WHERE", testing_workload[0])
 table = m.groups()[0]
-workload = m.groups()[1]
-selectivity = m.groups()[2]
-m = re.match("(.+)([1-9]+)", table)
-dataset = m.groups()[0]
-dimensions = int(m.groups()[1])
-
-if "set1" in table:
-   table = table.replace("set1", "set1_")
-
-# Determine the total volume of the given table.
-if not args.reuse:
-  cur.execute("DELETE FROM pg_kdemodels;");
-sys.stdout.flush()
-# Also count the number of rows in the table.
-cur.execute("SELECT COUNT(*) FROM %s;" % table)
-nrows = int(cur.fetchone()[0])
-
-# We will now select a random training and test file. For this,
-# we first need to find out how many queries there are in total.
-f = open(queryfile)
-for total_queries, _ in enumerate(f):
-    pass
-total_queries += 1
-# Now we do a quick sanity check.
-if (queries > total_queries or trainqueries > total_queries):
-    print "Requested more queries than available."
-    sys.exit(-1)
-# Ok, now select which queries are in the training and which are in the test set.
-selected_training_queries = range(1, total_queries)
-random.shuffle(selected_training_queries)
-selected_training_queries = set(selected_training_queries[0:trainqueries])
-selected_test_queries = range(1, total_queries)
-random.shuffle(selected_test_queries)
-selected_test_queries = set(selected_test_queries[0:queries])
-# If we run heuristic or optimal KDE, we do not need a training set.
-if (model == "kde_heuristic" or model == "kde_optimal"):
-    trainqueries = 0
-
-# Set the requested error metric.
-if (errortype == "relative"):
-    cur.execute("SET kde_error_metric TO SquaredRelative;")
-elif (errortype == "absolute"):
-    cur.execute("SET kde_error_metric TO Quadratic;")
-# Set STHoles specific parameters.
-if (model == "stholes"):
-    cur.execute("SET stholes_hole_limit TO %i;" % modelsize)
-    cur.execute("SET stholes_enable TO true;")
-    createModel(table, dimensions,False)
+cur.execute("SELECT count(*) FROM information_schema.columns WHERE table_name='%s';" % table)
+dimensions = int(cur.fetchone()[0])
+if args.replay_experiment:
+    workload_type = '_' # Marker for replayed workloads.
 else:
-    # Set KDE-specific parameters.
-    cur.execute("SET kde_samplesize TO %i;" % modelsize)
-    if not usegpu:
-      cur.execute("SET ocl_use_gpu TO false;")
+    # Extract the workload type from the query file name:
+    m = re.match("(.+)_([a-z]+)_(.+).sql", ntpath.basename(args.queryfile))
+    workload_type = m.groups()[1]
+
+# Remove all existing model traces if we don't reuse the model.
+cur.execute("DELETE FROM pg_kdemodels;")
+cur.execute("DELETE FROM pg_kdefeedback;")
+cur.execute("SELECT pg_stat_reset();")
+
+# Make sure we only run training queries if we have an adaptive model:
+if (args.model in ("kde_heuristic", "kde_scv", "postgres", "none")):
+    training_workload = []
+# Set required KDE model parameters.
+if "kde" in args.model:
+    # KDE-specific parameters.
+    cur.execute("SET kde_samplesize TO %i;" % args.modelsize)
     cur.execute("SET kde_enable TO true;")
-if uselogbw:
-    cur.execute("SET kde_bandwidth_representation TO Log;")
-    
-# Initialize the training phase.
-if (model == "kde_batch"):
-    # Drop all existing feedback and start feedback collection.
-    cur.execute("DELETE FROM pg_kdefeedback;")
+    if args.logbw:
+        cur.execute("SET kde_bandwidth_representation TO Log;")
+
+# Initialize the training phase for batch, adaptive and stholes:
+if (args.model == "kde_batch" ):
     cur.execute("SET kde_collect_feedback TO true;")
-elif (model == "kde_adaptive_rmsprop"):
+elif (args.model == "kde_adaptive"):
     # Build an initial model that is being trained.
     cur.execute("SET kde_enable_adaptive_bandwidth TO true;")
     cur.execute("SET kde_minibatch_size TO 10;")
     cur.execute("SET kde_online_optimization_algorithm TO rmsprop;")
-    createModel(table, dimensions,args.reuse)
-elif (model == "kde_adaptive_vsgd"):
-    # Build an initial model that is being trained.
-    cur.execute("SET kde_enable_adaptive_bandwidth TO true;")
-    cur.execute("SET kde_minibatch_size TO 10;")
-    cur.execute("SET kde_online_optimization_algorithm TO vSGDfd;")
-    createModel(table, dimensions,args.reuse)
-# Now run the training queries.
-f.seek(0)
-finished_queries = 0
-if args.record: 
-  train_queries_f = open("/tmp/train_queries_%s.log" % args.dbname, "w")
-  train_selectivities_f = open("/tmp/train_selectivities_%s.log" % args.dbname, "w")
-if (trainqueries > 0):
-    sys.stdout.write("\tRunning training queries ... ") 
+    analyzeTable(args, table, dimensions)
+elif (args.model == "stholes"):
+    # STHoles-specific parameters.
+    cur.execute("SET stholes_hole_limit TO %i;" % (args.modelsize / 2))
+    cur.execute("SET stholes_enable TO true;")
+    analyzeTable(args, table, dimensions)
+
+# Run the training workload.
+if training_workload:
+    sys.stdout.write("\tRunning training queries ... ")
     sys.stdout.flush()
-    for linenr, line in enumerate(f):
-        if linenr in selected_training_queries:
-            cur.execute(line)
-            if args.record: 
-               train_queries_f.write(line)
-               train_selectivities_f.write("%s\n" % cur.fetchone()[0])
-            finished_queries += 1
-    print "done!"
-if (model == "kde_batch"):
+    ts = time.time()
+    for query in training_workload:
+       cur.execute(query)
+    print "done (took %.2f ms)!" % (1000*(time.time() - ts))
+
+# Build batch models based on the collected feedback.
+if (args.model == "kde_batch"):
     cur.execute("SET kde_collect_feedback TO false;") # We don't need further feedback collection.    
     cur.execute("SET kde_enable_bandwidth_optimization TO true;")
-    cur.execute("SET kde_optimization_feedback_window TO %i;" % trainqueries)
-if (model != "kde_adaptive_rmsprop" and model != "kde_adaptive_vsgd" and model != "stholes"):
-    createModel(table, dimensions,args.reuse)
-# If this is the optimal estimator, we need to compute the PI bandwidth estimate.
-if (model == "kde_optimal"):
-  sys.stdout.write("\tImporting data sample into R ... ")
-  sys.stdout.flush()
-  cur.execute("SELECT kde_dump_sample('%s', '/tmp/sample_%s.csv');" % (table, args.dbname))
-  m = robjects.r['read.csv']('/tmp/sample_%s.csv' % args.dbname)
-  sys.stdout.write("done!\n\tCalling SCV bandwidth estimator ... ")
-  sys.stdout.flush()
-  ks = importr("ks")
-  bw = robjects.r['diag'](robjects.r('Hscv.diag')(m))
-  bw_array = 'ARRAY[%f' % bw[0]
-  for v in bw[1:]:
-    bw_array += ',%f' % v
-  bw_array += ']'
-  cur.execute("SELECT kde_set_bandwidth('%s',%s);" % (table, bw_array))
-  print "\tSetting bandwidth estimate: ", bw
+    cur.execute("SET kde_optimization_feedback_window TO %i;" % len(training_workload))
+# KDE Adaptive and STHOles have already built their model, now we trigger building for all other models.
+if (args.model != "stholes" and args.model != "kde_adaptive"):
+    analyzeTable(args, table, dimensions)
+# Finally, for SCV models, we use the exported sample to compute the optimal bandwidth via SCV
+if (args.model == "kde_scv"):
+    sample_file = "/tmp/sample_%s.csv" % args.dbname
+    sys.stdout.write("\tImporting data sample into R ... ")
+    sys.stdout.flush()
+    m = robjects.r['read.csv']('%s' % sample_file)
+    sys.stdout.write("done!\n\tCalling SCV bandwidth estimator ... ")
+    sys.stdout.flush()
+    ts = time.time()
+    ks = importr("ks")
+    bw = robjects.r['diag'](robjects.r('Hscv.diag')(m))
+    bw_array = 'ARRAY[%f' % bw[0]
+    for v in bw[1:]:
+        bw_array += ',%f' % v
+    bw_array += ']'
+    print "done (took %.2f ms)!" % (1000 * (time.time() - ts))
+    cur.execute("SELECT kde_set_bandwidth('%s',%s);" % (table, bw_array))
 
-if (args.record):
-  train_queries_f.close()
-  train_selectivities_f.close()
-  # Dump the model sample.
-  if (model != "stholes"):
-    cur.execute("SELECT kde_dump_sample('%s', '/tmp/sample_%s.csv');" % (table, args.dbname))
-  # Prepare to dump the run queries.
-  wf = open("/tmp/test_queries_%s.log" % args.dbname, "w")
-
-# And run the experiments.
-f.seek(0)
-allrows = 0
-sys.stdout.write("\tRunning experiment ... ")
+# Ok, we are all set. Run the experiments!
+f = open(args.log, "a+")
+sys.stdout.write("\tRunning test queries ... ")
 sys.stdout.flush()
-flog = open(log, "a+")
-for linenr, line in enumerate(f):
-    if linenr in selected_test_queries:
-        cur.execute("EXPLAIN ANALYZE %s" % line)
-        for row in cur:
-          text = row[0]
-          if "Seq Scan" in text:
+ts = time.time()
+for query in testing_workload:
+    cur.execute("EXPLAIN ANALYZE %s" % query)
+    # Extract the true and estimated result row count from the explain result
+    for row in cur:
+        text = row[0]
+        if "Seq Scan" in text:
             m = re.match(".+rows=([0-9]+).+rows=([0-9]+).+", text)
-            error = abs(int(m.group(1)) - int(m.group(2)))
-            flog.write("%s;%i;%s;%s;%i;%i\n" % (dataset, dimensions, workload, model, modelsize, error))
-        if args.record:
-          wf.write("%s" % line)
+            predicted = int(m.group(1))
+            actual = int(m.group(2))
+            f.write("%s;%i;%s;%s;%i;%i;%i\n" % (table, dimensions, workload_type, args.model, args.modelsize, predicted, actual))
+print "done (took %.2f ms)!" % (1000*(time.time() - ts))
 
-print "done!"
-wf.close()
+# Clean up
 f.close()
-flog.close()
 conn.close()
-    
-if args.record:
-   wf.close()
