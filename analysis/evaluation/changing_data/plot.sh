4b7a7f336c50a38d53e9d03b795f6046f2e07b05
Martin Kiefer <martinkiefer@boxm.de>
2015-09-08 15:23:29 +0200

    Minor changes to the plot.sh script to make it callable outside of the current directory.

--- analysis/evaluation/changing_data/plot.sh
+++ analysis/evaluation/changing_data/plot.sh
@@ -2,14 +2,15 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/../../conf.sh
-echo $DIR/5_adaptive_raw.log
-python $DIR/analyze.py $DIR/5_heuristic_raw.log > 5_heuristic_aggregated.log
-python $DIR/analyze.py $DIR/5_adaptive_tkr_raw.log > 5_adaptive_tkr_aggregated.log
-python $DIR/analyze.py $DIR/5_stholes_raw.log > 5_stholes_aggregated.log
 
-python $DIR/analyze.py $DIR/8_heuristic_raw.log > 8_heuristic_aggregated.log
-python $DIR/analyze.py $DIR/8_adaptive_tkr_raw.log > 8_adaptive_tkr_aggregated.log
-python $DIR/analyze.py $DIR/8_stholes_raw.log > 8_stholes_aggregated.log
+cd $DIR
+python analyze.py 5_heuristic_raw.log > 5_heuristic_aggregated.log
+python analyze.py 5_adaptive_tkr_raw.log > 5_adaptive_tkr_aggregated.log
+python analyze.py 5_stholes_raw.log > 5_stholes_aggregated.log
+
+python analyze.py 8_heuristic_raw.log > 8_heuristic_aggregated.log
+python analyze.py 8_adaptive_tkr_raw.log > 8_adaptive_tkr_aggregated.log
+python analyze.py 8_stholes_raw.log > 8_stholes_aggregated.log
 
 gnuplot plot5.gnuplot
 gnuplot plot8.gnuplot
