b285a1de9830f5d1c0592a7bc86f3cf153bac23e
Martin Kiefer <martinkiefer@boxm.de>
2015-09-10 19:13:45 +0200

    Minor fix.

--- Check.sh
+++ Check.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 source ./analysis/conf.sh
 
-PGBIN=$PGINSTFOLDE/bin
+PGBIN=$PGINSTFOLDER/bin
 $PGBIN/pg_ctl -D $PGDATAFOLDER -l logfile start
 sleep 3
-$PYTHON --dbname $PGDATABASE --port $PGPORT
+$PYTHON Check.py --dbname $PGDATABASE --port $PGPORT
 $PGBIN/pg_ctl -D $PGDATAFOLDER -l logfile stop
