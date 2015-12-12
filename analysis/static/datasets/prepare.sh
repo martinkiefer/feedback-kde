174c80603105fbbf0bb8a255b6a923acfcc548da
Max Heimel <mheimel@googlemail.com>
2015-12-12 02:18:15 -0800

    Query generation now uses SQlite.

--- analysis/static/datasets/prepare.sh
+++ analysis/static/datasets/prepare.sh
@@ -12,37 +12,38 @@ for dataset in "${DATASETS[@]}" ; do
   $DIR/$dataset/load.sh
   # Now build the queries.
   mkdir -p $DIR/$dataset/queries
+  mkdir -p $DIR/$dataset/data
   source $DIR/$dataset/tables.sh
   for table in "${TABLES[@]}"; do
-    echo "Generating workload for table $table ... "
+    echo "COPY $table TO '$DIR/$dataset/data/$table.csv' DELIMITER '|';" | $PSQL $PGDATABASE
     # Generate the dv query set (data centered, target volume).
-    $PYTHON $DIR/query_generator.py                  \
-     --dbname=$PGDATABASE --port=$PGPORT            \
-     --table=$table --queries=$QUERIES              \
+    echo "Generating DV workload for table $table ... "
+    $PYTHON $DIR/query_generator2.py                 \
+     --data=$DIR/$dataset/data/$table.csv --queries=$QUERIES \
      --selectivity=0.01 --mcenter=Data              \
      --mrange=Volume                                \
      --out=$DIR/$dataset/queries/${table}_dv_0.01.sql &
     DVPID=$!
     # Generate the uv query set (uniform centers, target volume).
-    $PYTHON $DIR/query_generator.py                  \
-     --dbname=$PGDATABASE --port=$PGPORT            \
-     --table=$table --queries=$QUERIES              \
+    echo "Generating UV workload for table $table ... "
+    $PYTHON $DIR/query_generator2.py            \
+     --data=$DIR/$dataset/data/$table.csv --queries=$QUERIES \
      --selectivity=0.01 --mcenter=Uniform           \
      --mrange=Volume                                \
      --out=$DIR/$dataset/queries/${table}_uv_0.01.sql &
     UVPID=$!
     # Generate the dt query set (data centered, target selectivity).
-    $PYTHON $DIR/query_generator.py                  \
-     --dbname=$PGDATABASE --port=$PGPORT            \
-     --table=$table --queries=$QUERIES              \
+    echo "Generating DT workload for table $table ... "
+    $PYTHON $DIR/query_generator2.py            \
+     --data=$DIR/$dataset/data/$table.csv --queries=$QUERIES \
      --selectivity=0.01 --mcenter=Data              \
      --mrange=Tuples                                \
      --out=$DIR/$dataset/queries/${table}_dt_0.01.sql &
     DTPID=$!
     # Generate the ut query set (uniform centers, target selectivity).
-    $PYTHON $DIR/query_generator.py                  \
-     --dbname=$PGDATABASE --port=$PGPORT            \
-     --table=$table --queries=$QUERIES              \
+    echo "Generating UT workload for table $table ... "
+    $PYTHON $DIR/query_generator2.py            \
+     --data=$DIR/$dataset/data/$table.csv --queries=$QUERIES \
      --selectivity=0.01 --mcenter=Uniform           \
      --mrange=Tuples                                \
      --out=$DIR/$dataset/queries/${table}_ut_0.01.sql &
@@ -62,7 +63,7 @@ done
 #  for table in "${TABLES[@]}"; do
 #    cd $dataset/queries
 #    $PYTHON $DIR/scaleExperiments.py                 \
-#      --dbname=$PGDATABASE --port=$PGPORT           \
+#      --dbname=$PGDATABASE --port=$PGPORT           #\
 #      --queryfile=${table}_ut_0.01.sql
 #    $PYTHON $DIR/scaleExperiments.py                 \
 #      --dbname=$PGDATABASE --port=$PGPORT           \
--- analysis/static/datasets/query_generator.py
+++ analysis/static/datasets/query_generator.py
@@ -1,25 +1,35 @@
+#!/usr/bin/env python
+
 # -*- coding: utf-8 -*-
 import argparse
+import csv
 import itertools
 import math
-from numpy import random
-import numpy
+import numpy as np
 import os
-import psycopg2
+import sqlite3
 import sys
 import time
 
-#Classes generating data centers (Data,Uniform,Gauss)
+from numpy import random
+
+total_queries = 0
+def run(cur, query, parameters):
+   global total_queries
+   total_queries += 1
+   return cur.execute(query, parameters)
+
+# Classes generating data centers (Data,Uniform,Gauss)
 class DataCenterGenerator:
-    def __init__(self,cur,table,rows):
-        self._cur = cur
-        self._table = table
-        self._rows = rows
-        self._cur.execute("SELECT * FROM %s ORDER BY RANDOM()" % self._table)
-          
+    def __init__(self, cur, rows):
+        # Fetch the dataset into memory.
+        self.data = []
+        for row in cur.execute("SELECT * FROM d;"):
+           self.data.append(list(row))
+        self.rows = rows
+
     def getNextCenter(self):
-        self._cur.scroll(random.randint(0,rows),'absolute')
-        return [list(self._cur.fetchone())] 
+        return self.data[random.randint(0, self.rows)]
 
 class UniformDataCenterGenerator:
     def __init__(self, low,high,columns):
@@ -28,7 +38,7 @@ class UniformDataCenterGenerator:
         self._columns = columns
         
     def getNextCenter(self):
-        return self._low + (self._high-self._low) * random.random_sample((1,self._columns))
+        return self._low + (self._high - self._low) * random.random_sample(self._columns)
             
 class GaussCenterGenerator:
     def __init__(self, low, high, clusters,columns,sigma):
@@ -48,9 +58,9 @@ class GaussCenterGenerator:
         for delta in random.normal(0,self._sigma,(1,self._columns)):
             return center+random.normal(0,self._sigma,(1,self._columns))
 
-def createBoundsList(min,max):
+def createBoundsList(min_vals, max_vals):
     result = []
-    for x,y in itertools.izip(min[0],max[0]):
+    for x,y in itertools.izip(min_vals, max_vals):
         if (x == y):
           x -= 0.0001;
           y += 0.0001;
@@ -58,12 +68,44 @@ def createBoundsList(min,max):
         result.append(y)
     return result
 
-  
+def parseData(c, data_file):
+   # Open the data file and read the first line.
+   data = []   
+   with open(data_file, "r") as f:
+      reader = csv.reader(f, delimiter='|')
+      for row in reader:
+         data.append([float(x) for x in row])
+   return data      
+
+def loadData(data):
+   dim = len(data[0])
+   # Create the table.
+   query = "CREATE TABLE d("
+   for i in range(dim):
+      if (i > 0):
+         query += ", "
+      query += "c%i FLOAT" % i
+   query += ");"
+   cur.execute(query)
+   # Run the bulk insert.
+   query = "INSERT INTO d("
+   for i in range(dim):
+      if (i > 0):
+         query += ", "
+      query += "c%i" % i
+   query += ") VALUES ("
+   for i in range(dim):
+      if (i > 0):
+         query += ", "
+      query += "?"
+   query += ");"
+   cur.executemany(query, data)
+   cur.execute("SELECT count(*) FROM d;")
+   return dim
+ 
 # Define and parse the command line arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("--dbname", action="store", required=True, help="Database to which the script will connect.")
-parser.add_argument("--port", action="store", type=int, default=5432, help="Port of the postmaster.")
-parser.add_argument("--table", action="store", required=True, help="Table for which the workload will be generated.")
+parser.add_argument("--data", action="store", required=True, help="CSV File containing the input data.")
 parser.add_argument("--selectivity", action="store", required=True, type=float, help="Target selectivity for the created query workload.")
 parser.add_argument("--mcenter", action="store", choices=["Data","Uniform","Gauss"],required=True, help="Mechanism choosing the center of range queries")
 parser.add_argument("--mrange", action="store", choices=["Volume","Tuples"],required=False, help="Mechanism determining the width of the range queries")
@@ -75,7 +117,6 @@ parser.add_argument("--output", action="store", required=True, help="Name of the
 args = parser.parse_args()
 
 # Fetch them.
-table = args.table
 target_selectivity = args.selectivity
 target_tolerance = args.tolerance
 sigma = args.sigma
@@ -87,66 +128,51 @@ mrange = args.mrange
 
 output_file_name = os.path.basename(output_file)
 
-conn = psycopg2.connect("dbname=%s host=localhost port=%i" % (args.dbname, args.port))
+conn = sqlite3.connect(":memory:", isolation_level=None) 
 cur = conn.cursor()
+data = parseData(cur, args.data)
+columns = len(data[0])
+rows = len(data)
 
-# Figure out the dimensionality of this table.
-cur.execute("SELECT COUNT(*) FROM information_schema.columns WHERE table_name='%s'" % table)
-columns = cur.fetchone()[0]
+# Extract minimum and maximum values.
+low = np.amin(data, axis=0)
+high = np.amax(data, axis=0)
+ranges = zip(low, high)
 
-# And the total number of rows in the table.
-cur.execute("SELECT COUNT(*) FROM %s" % table)
-rows = cur.fetchone()[0]
-
-ranges = []
-low = []
-high =  []
-for i in range(0, columns):
-    # We need indexes to achieve reasonably good performance.
-    try:
-    	cur.execute("CREATE INDEX %s_c%i ON %s(c%i)" % (table, i+1, table, i+1))
-        conn.commit()
-    except Exception, e:
-        conn.reset()
-        pass
-    # Fetch the minimum / maximum for each column to determine the query range.
-    # and calculate the edge of the hypercube incrementally.
-    cur.execute("SELECT MIN(c%i), MAX(c%i) FROM %s" % (i+1, i+1, table))
-    result = cur.fetchone()
-    low.append(result[0])
-    high.append(result[1])  
-    ranges.append([result[0], result[1] ])
-
-low = numpy.array(low)
-high =  numpy.array(high)
+# Now generate the query centers.
 centers = []
-
-cur.close()
-# Tell Postgres that we don't need transaction support for the rest of the script.
-conn.commit()
-conn.set_session('read uncommitted', readonly=True, autocommit=True)
-
 generator = None
 
-if(mcenter == "Data"):
-    generator = DataCenterGenerator(conn.cursor(),table,rows)
+# Check if we need to load the data.
+if (mcenter == "Data" or mrange == "Tuples"):
+   loadData(data)
+   # Build an composite index.
+   query = "CREATE INDEX d_idx ON d("
+   for i in range(0, columns):
+      if i>0:
+         query += ", "
+      query += "c%i" % i
+   query += ")"
+   cur.execute(query)
+   conn.commit()
 
-elif(mcenter == "Uniform"):
-    generator = UniformDataCenterGenerator(low, high, columns)
-
-elif(mcenter == "Gauss"):
-    generator = GaussCenterGenerator(low,high,clusters,columns,sigma)
-
+if (mcenter == "Data"):
+   # Load the data.
+   generator = DataCenterGenerator(conn.cursor(), rows)
+elif (mcenter == "Uniform"):
+   generator = UniformDataCenterGenerator(low, high, columns)
+elif (mcenter == "Gauss"):
+   generator = GaussCenterGenerator(low, high, clusters, columns, sigma)
 else:
-    print("Not yet implemented")
+   print("Not yet implemented")
 
 workload = []
     # Build the query template.
-template = "SELECT count(*) FROM %s WHERE " % table
+template = "SELECT count(*) FROM d WHERE "
 for i in range(0, columns):
     if i>0:
         template += "AND "
-    template += "c%d>%%s AND c%d<%%s " % (i+1, i+1)
+    template += "c%d>? AND c%d<? " % (i, i)
         
 if (mrange == "Volume"): 
     bounds = []
@@ -159,30 +185,28 @@ if (mrange == "Volume"):
 
     i = 0
     while (len(workload) < queries):
-        r = random.random_sample((1,columns))
+        r = random.random_sample(columns)
         c = generator.getNextCenter()
         workload.append(createBoundsList(c - 0.5 * edge * r, c + 0.5 * edge * r))
        
 elif (mrange == "Tuples"):    
-    last_query_batch = 0
 
     last_len = 0
+    start_time = time.time()
     last_print_time = time.time()
 
     while (len(workload) < queries):
         c = generator.getNextCenter()
-	
-        # Take the maximum range
-        ranges = 1.2*numpy.minimum(c - low, high - c)
-        
-        cur = conn.cursor()
-        cur.execute(template, map(str, createBoundsList(c - ranges, c + ranges)))
 
+        # Figure out how much space we have until we hit the boundary.
+        ranges = 1.1 * np.minimum(c - low, high - c)
+
+        # Now count how many points fall in this region.
+        run(cur, template, createBoundsList(c - ranges, c + ranges))
         selectivity = cur.fetchone()[0] / float(rows)
 
-        cur.close()
-        last_query_batch += 1
-        if (selectivity < (target_selectivity - 0.5*target_tolerance)):
+        # If the query region is too small, abort directyl. 
+        if (selectivity < (target_selectivity - 0.5 * target_tolerance)):
             continue
 	
         lower_bound = 0 
@@ -191,15 +215,13 @@ elif (mrange == "Tuples"):
         upper_bound_factor = 1 
         test_factor = 1
         if (selectivity < (target_selectivity + 0.5*target_tolerance) and selectivity > (target_selectivity - 0.5*target_tolerance)):
-            workload.append(createBoundsList(c-(ranges*test_factor),c+(ranges*test_factor)))  
+            workload.append(createBoundsList(c - (ranges*test_factor), c + (ranges*test_factor)))  
             continue
+        # Run a binary search to find the optimal query region.
         while (upper_bound - lower_bound > target_tolerance and (upper_bound_factor - lower_bound_factor) > 0.001 ):
-            last_query_batch += 1
             test_factor = 0.5 * (lower_bound_factor + upper_bound_factor)
-            cur = conn.cursor()
-            cur.execute(template, createBoundsList(c-(ranges*test_factor),c+(ranges*test_factor)))
+            run(cur, template, createBoundsList(c - (ranges*test_factor), c + (ranges*test_factor)))
             selectivity = cur.fetchone()[0] / float(rows)
-            cur.close()
             
             if (selectivity < (target_selectivity + 0.5*target_tolerance) and selectivity > (target_selectivity - 0.5*target_tolerance)):
                 break;
@@ -208,25 +230,25 @@ elif (mrange == "Tuples"):
                 upper_bound_factor = test_factor
             else:
                 lower_bound = selectivity
-                lower_bound_factor = test_factor 
+                lower_bound_factor = test_factor
         if (selectivity < (target_selectivity + 0.5*target_tolerance) and selectivity > (target_selectivity - 0.5*target_tolerance)):           
             workload.append(createBoundsList(c-(ranges*test_factor),c+(ranges*test_factor)))  
-        if (time.time() - last_print_time >= 2):
-            # Every two seconds, we print the current status.
-            for i in range(0,100):
+        if (time.time() - last_print_time >= 1):
+            # Print the current status every second: 
+            for i in range(0,140):
                  sys.stdout.write(" ")
             sys.stdout.write("\r") # Clear the line
-            sys.stdout.write("Generated %i queries for %s, %i remaining (%.1f queries / second)\r" \
+            sys.stdout.write("\tGenerated %i queries for %s, %i remaining (%.2f queries/s, %.2f database accesses / query)\r" \
                                   % (len(workload), output_file_name, queries - len(workload), \
-                                    (len(workload) - last_len) / float(2)))
+                                     (len(workload) / float(time.time() - start_time)), \
+                                     (total_queries / float(len(workload)))))
             sys.stdout.flush()
-            last_query_batch = 0
             last_print_time = time.time()
             last_len = len(workload)
     sys.stdout.write("\n")    
 
 # Prepare writing out the result to disk.
-template = template.replace("%s", "%f")
+template = template.replace("?", "%f")
 template += ";\n"
 
 f = open(output_file, "w")
@@ -235,17 +257,4 @@ for query in workload:
     f.write(template % tuple(query))
 f.close()
 
-# Finally, delete the generated indexes.
-conn.commit()
-conn.set_session('read uncommitted', readonly=False, autocommit=True)
-cur = conn.cursor()
-for i in range(0, columns):
-    # We need indexes to achieve reasonably good performance.
-    try:
-    	cur.execute("DROP INDEX %s_c%i" % (table, i+1))
-        conn.commit()
-    except Exception, e:
-        conn.reset()
-        pass
-cur.close()
 conn.close()
